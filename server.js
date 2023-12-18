0eae1010de3eeeca7cbe5e137f1055e3feb7e93c
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-18 19:31:32 +0530

    commented context !

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 const app = require('./app');
 const router = require('./router');
 
-/*var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "80221474",
-    insecureAuth : true
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});*/
 const server = app.listen(3001,'127.0.0.1',()=>{
     console.log('listening to server !');
 });
-
+// set context !
 app.use('/api',router);
